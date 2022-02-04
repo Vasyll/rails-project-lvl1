ed622b9ee20cb788df315f29ce9ea4047b528032
Vyacheslav Ryzhov <chjusp@gmail.com>
2022-02-04 14:53:54 +0300

    4 commit

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,15 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  module Tag
+    def self.build(tag, options = {})
+      return "<br>" if tag == "br"
+      return "<img src=\"#{options[:src]}\">" if tag == "img"
+      return "<input type=\"#{options[:type]}\" value=\"#{options[:value]}\">" if tag == "input"
+      return "<label>#{yield}</label>" if tag == "label" && options[:for].nil?
+      return "<label for=\"#{options[:for]}\">#{yield}</label>" if tag == "label"
+      return "<div></div>" if tag == "div"
+    end
+  end
 end
--- test/hexlet_code_test.rb
+++ test/hexlet_code_test.rb
@@ -7,7 +7,12 @@ class HexletCodeTest < Minitest::Test
     refute_nil ::HexletCode::VERSION
   end
 
-# def test_it_does_something_useful
-#   assert false
-# end
+  def test_it_does_something_useful
+    assert_match("<br>", HexletCode::Tag.build("br"))
+    assert_match('<img src="path/to/image">', HexletCode::Tag.build("img", src: "path/to/image"))
+    assert_match('<input type="submit" value="Save">', HexletCode::Tag.build("input", type: "submit", value: "Save"))
+    assert_match("<label>Email</label>", HexletCode::Tag.build("label") { "Email" })
+    assert_match('<label for="email">Email</label>', HexletCode::Tag.build("label", for: "email") { "Email" })
+    assert_match("<div></div>", HexletCode::Tag.build("div"))
+  end
 end
