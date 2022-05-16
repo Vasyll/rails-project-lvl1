29a0aeb483c2cbdc67e2d40d12eed65fbff1e3a3
Vyacheslav Ryzhov <chjusp@gmail.com>
2022-05-16 16:22:11 +0300

    Delete Extra blank line

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -7,7 +7,6 @@ module HexletCode
   autoload :FormData, 'hexlet_code/form_data'
   autoload :Tag, 'hexlet_code/tag'
 
-
   def self.form_for(record, options = {})
     form_data = FormData.new(record, options)
     yield(form_data)
